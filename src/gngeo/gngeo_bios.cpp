11717a7d200f5219e04487e25e2774fc914d3639
Jefferson <jeff.ssa@proton.me>
2026-09-16 18:43:40 -0300

    gngeo: implement 000-lo.lo BIOS loading

---
+++ src/gngeo/gngeo_bios.cpp
@@ -0,0 +1,63 @@
+#include <mednafen/mednafen.h>
+#include "../compress/ArchiveReader.h"
+#include "gngeo_bios.h"
+
+namespace Mednafen
+{
+
+static Uint8 *GnGeoLoBios = nullptr;
+static size_t GnGeoLoBiosSize = 0;
+
+bool GnGeoLoadBiosLo(GameFile *gf)
+{
+    (void)gf;
+    GnGeoFreeBiosLo();
+
+    std::unique_ptr<ArchiveReader> archive(
+        ArchiveReader::Open(
+            &NVFS,
+            MDFN_MakeFName(MDFNMKF_FIRMWARE, 0, "neogeo.zip")));
+
+    if(!archive)
+        return false;
+
+    std::unique_ptr<Stream> stream;
+
+    try
+    {
+        stream.reset(archive->open("000-lo.lo", VirtualFS::MODE_READ));
+    }
+    catch(const MDFN_Error&)
+    {
+        return false;
+    }
+
+    if(!stream)
+        return false;
+
+    GnGeoLoBiosSize = (size_t)stream->size();
+    GnGeoLoBios = (Uint8 *)malloc(GnGeoLoBiosSize);
+
+    if(!GnGeoLoBios)
+    {
+        GnGeoLoBiosSize = 0;
+        return false;
+    }
+
+    if(stream->read(GnGeoLoBios, GnGeoLoBiosSize) != GnGeoLoBiosSize)
+    {
+        GnGeoFreeBiosLo();
+        return false;
+    }
+
+    return true;
+}
+
+void GnGeoFreeBiosLo(void)
+{
+    free(GnGeoLoBios);
+    GnGeoLoBios = nullptr;
+    GnGeoLoBiosSize = 0;
+}
+
+}
