1872d353d418928fd107b962663499aeac6c33ae
Jefferson <jeff.ssa@proton.me>
2026-09-20 21:46:44 -0300

    Expose GnGeo core shutdown

--- src/gngeo/core/gngeo_runtime.h
+++ src/gngeo/core/gngeo_runtime.h
@@ -3,5 +3,5 @@
 #include <stddef.h>
 #include <stdint.h>
 typedef struct GAME_ROMS GAME_ROMS;
-void init_neo(void); int GnGeoCoreInitRoms(void); void setup_misc_patch(char*); void neogeo_reset(void); void GnGeoCoreSetRoms(GAME_ROMS*,uint8_t*); void GnGeoCoreSetInput(uint8_t,uint8_t,uint8_t,uint8_t,uint8_t); void GnGeoCoreGetFrame(uint16_t*,unsigned); void GnGeoCoreSetAudioRate(int); unsigned GnGeoCoreGenerateAudio(int16_t*,unsigned); int GnGeoRunFrame(void); size_t GnGeoCoreStateSize(void); int GnGeoCoreStateSaveLoad(void*,int);
+void init_neo(void); void GnGeoCoreShutdown(void); int GnGeoCoreInitRoms(void); void setup_misc_patch(char*); void neogeo_reset(void); void GnGeoCoreSetRoms(GAME_ROMS*,uint8_t*); void GnGeoCoreSetInput(uint8_t,uint8_t,uint8_t,uint8_t,uint8_t); void GnGeoCoreGetFrame(uint16_t*,unsigned); void GnGeoCoreSetAudioRate(int); unsigned GnGeoCoreGenerateAudio(int16_t*,unsigned); int GnGeoRunFrame(void); size_t GnGeoCoreStateSize(void); int GnGeoCoreStateSaveLoad(void*,int);
 #endif
