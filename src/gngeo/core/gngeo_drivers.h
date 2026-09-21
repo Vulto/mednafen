da6c735ceda3c5e5a6acd0e3a177414f0e1365df
Jefferson <jeff.ssa@proton.me>
2026-09-20 21:34:54 -0300

    gngeo: expose driver lookup with C linkage

--- src/gngeo/core/gngeo_drivers.h
+++ src/gngeo/core/gngeo_drivers.h
@@ -1,5 +1,11 @@
 #ifndef GNGEO_DRIVERS_H
 #define GNGEO_DRIVERS_H
 #include "gngeo_compat.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 const void *GnGeoFindDriver(const char*,Uint32*);
+#ifdef __cplusplus
+}
+#endif
 #endif
