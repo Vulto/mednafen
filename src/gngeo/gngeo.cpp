84b2882d0ec870b41f95ce864b0993b2d96f99c6
Jefferson <jeff.ssa@proton.me>
2026-09-22 14:08:33 -0300

    gngeo: use external driver detection

--- src/gngeo/gngeo.cpp
+++ src/gngeo/gngeo.cpp
@@ -88,20 +88,7 @@ static void Load(GameFile *gf)
 
 static bool TestMagic(GameFile *gf)
 {
- if(!gf || gf->outside.vfs == nullptr || gf->outside.fbase.empty()) return false;
-
- Uint32 size = 0;
- if(GnGeoFindDriver(gf->outside.fbase.c_str(), &size) != nullptr && size != 0)
-  return true;
-
- std::string lower = gf->outside.fbase;
- for(char &c : lower)
-  c = (char)std::tolower((unsigned char)c);
-
- if(lower != gf->outside.fbase && GnGeoFindDriver(lower.c_str(), &size) != nullptr && size != 0)
-  return true;
-
- return false;
+ return GnGeoHasDriver(gf);
 }
 
 static void CloseGame(){ GnGeoCoreShutdown(); StateBlob.clear(); GnGeoFreeRoms(&GameRoms); GnGeoFreeBiosLo(); memset(&GameRoms,0,sizeof(GameRoms)); }
