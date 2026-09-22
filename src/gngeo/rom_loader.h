295a8f6c37aea3fc6c569f0ebd5eee3b16617ca4
Jefferson <jeff.ssa@proton.me>
2026-09-22 14:08:30 -0300

    gngeo: expose driver detection

--- src/gngeo/rom_loader.h
+++ src/gngeo/rom_loader.h
@@ -2,5 +2,5 @@
 #define MDFN_GNGEO_ROM_LOADER_H
 #include "rom_types.h"
 void GnGeoFreeRoms(GAME_ROMS* roms);
-namespace Mednafen { struct GameFile; bool GnGeoLoadRomSet(GameFile*, GAME_ROMS*, SYSTEM, COUNTRY); }
+namespace Mednafen { struct GameFile; bool GnGeoHasDriver(GameFile*); bool GnGeoLoadRomSet(GameFile*, GAME_ROMS*, SYSTEM, COUNTRY); }
 #endif
