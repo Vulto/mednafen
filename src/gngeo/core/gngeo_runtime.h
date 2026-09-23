4eb2121dffba390fb62685a7a68c12ca3c77def5
Jefferson <jeff.ssa@proton.me>
2026-09-22 23:31:15 -0300

    gngeo: expose Neo Geo system types to runtime API

--- src/gngeo/core/gngeo_runtime.h
+++ src/gngeo/core/gngeo_runtime.h
@@ -2,6 +2,7 @@
 #define GNGEO_RUNTIME_H
 #include <stddef.h>
 #include <stdint.h>
+#include "emu.h"
 typedef struct GAME_ROMS GAME_ROMS;
 void init_neo(void); void GnGeoCoreShutdown(void); int GnGeoCoreInitRoms(void); void setup_misc_patch(char*); void neogeo_reset(void); void GnGeoCoreSetRoms(GAME_ROMS*,uint8_t*,SYSTEM,COUNTRY); void GnGeoCoreSetInput(uint8_t,uint8_t,uint8_t,uint8_t,uint8_t); void GnGeoCoreGetFrame(uint16_t*,unsigned); void GnGeoCoreSetAudioRate(int); unsigned GnGeoCoreGenerateAudio(int16_t*,unsigned); int GnGeoRunFrame(void); size_t GnGeoCoreStateSize(void); int GnGeoCoreStateSaveLoad(void*,int);
 #endif
